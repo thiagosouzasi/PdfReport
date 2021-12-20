13af961caa49a8aa8467471a85ca87ef879cd6e0
ThiagoSouza <thiagosouzasi32@gmail.com>
2021-12-20 12:36:44 -0300

    Finalizando verificação de preenchimento de campos

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -12,9 +12,30 @@ export default function Home(){
 
     const makePDF = (type)=>{
         try {
-            Tools.genareteReport(title,subTitle,body,type);
+            if(verifyFilds()){
+                Tools.genareteReport(title,subTitle,body,type);
+            }
         } catch (error) {
             console.log(`Erro ao gerar pdf ->> ${error}`);
+        }
+    }
+    
+    const verifyFilds = ()=>{
+        try {
+            if(title === ''){
+                alert('Preencha o Título do relatório');
+                return false;
+            }else if(subTitle === ''){
+                alert('Preencha o Subtítulo do relatório');
+                return false;
+            }else if(body === ''){
+                alert('Preencha o Texto para o relatório');
+                return false;
+            }else{
+                return true;
+            }
+        } catch (error) {
+            console.log(`Erro ao gerar pdf ->> ${error}`);            
         }
     }
 
@@ -26,15 +47,15 @@ export default function Home(){
                 </div>
                 <div id="imputsReport">
                     <label htmlFor='login'>Título</label>
-                    <input placeholder='Informe o título do relatório' required  value={title} type='email' onChange={(e)=>{setTitle(e.target.value)}} />
+                    <input placeholder='Informe o título do relatório' required  value={title}  onChange={(e)=>{setTitle(e.target.value)}} />
                     <label htmlFor='login'>Subtítulo</label>
                     <input placeholder='Informe um subtítulo para o relatório'  required value={subTitle} onChange={(e)=>{setSubTitle(e.target.value)}}/>
                     <label htmlFor='login'>Texto</label>
                     <textarea value={body} type="text" rows="5"  onChange={(e)=>{setBody(e.target.value)}} placeholder='Digite o texto para o relatório'  required value={body} onChange={(e)=>{setBody(e.target.value)}} />
                 </div>
                 <div id="btnReport">
-                    <button type='submit' onClick={()=>{makePDF(0)}}>Baixar PDF</button>
-                    <button type='submit' onClick={()=>{makePDF(1)}} >Abrir em nova aba</button>
+                    <button  onClick={()=>{makePDF(0)}}>Baixar PDF</button>
+                    <button  onClick={()=>{makePDF(1)}} >Abrir em nova aba</button>
                 </div>
             </form>
         </main>
